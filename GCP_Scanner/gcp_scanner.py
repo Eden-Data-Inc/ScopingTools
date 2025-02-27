01be3f65967a6cf1c9b3f1ed1510de54e8711f86
ace-irinco <ace.irinco@mcg.llc>
2025-02-28 04:05:44 +0800

    added new functions

--- GCP_Scanner/gcp_scanner.py
+++ GCP_Scanner/gcp_scanner.py
@@ -21,31 +21,115 @@ def get_gcp_client(service_name, version, credentials):
     return build(service_name, version, credentials=credentials)
 
 def list_compute_instances(compute_client, project_id):
-    request = compute_client.instances().aggregatedList(project=project_id)
-    instances = []
-    while request is not None:
-        response = request.execute()
-        for zone, data in response.get("items", {}).items():
-            for instance in data.get("instances", []):
-                external_ip = None
-                for interface in instance.get("networkInterfaces", []):
-                    if "accessConfigs" in interface:
-                        for config in interface["accessConfigs"]:
-                            if "natIP" in config:
-                                external_ip = config["natIP"]
-                if external_ip:
-                    instances.append(external_ip)
-        request = compute_client.instances().aggregatedList_next(previous_request=request, previous_response=response)
-    return instances
+    try:
+        request = compute_client.instances().aggregatedList(project=project_id)
+        instances = []
+        while request is not None:
+            response = request.execute()
+            for zone, data in response.get("items", {}).items():
+                for instance in data.get("instances", []):
+                    external_ip = None
+                    for interface in instance.get("networkInterfaces", []):
+                        if "accessConfigs" in interface:
+                            for config in interface["accessConfigs"]:
+                                if "natIP" in config:
+                                    external_ip = config["natIP"]
+                    if external_ip:
+                        instances.append(external_ip)
+            request = compute_client.instances().aggregatedList_next(previous_request=request, previous_response=response)
+        return instances
+        
+    except:
+        return []
 
 def list_storage_buckets(storage_client, project_id):
-    request = storage_client.buckets().list(project=project_id)
-    response = request.execute()
-    return [bucket["name"] for bucket in response.get("items", [])]
+    try:
+        request = storage_client.buckets().list(project=project_id)
+        response = request.execute()
+        return [bucket["name"] for bucket in response.get("items", [])]
+    except:
+        return []
+
+def list_cloudsql_instances(sql_client, project_id):
+    try:
+        request = sql_client.instances().list(project=project_id)
+        response = request.execute()
+        return [instance["name"] for instance in response.get("items", [])]
+    except:
+        return []
+
+def list_pubsub_topics(pubsub_client, project_id):
+    try:
+        request = pubsub_client.projects().topics().list(project=f"projects/{project_id}")
+        response = request.execute()
+        return [topic.split("/")[-1] for topic in response.get("topics", [])]
+    except:
+        return []
+
+def list_cloud_functions(functions_client, project_id):
+    try:
+        request = functions_client.projects().locations().functions().list(parent=f"projects/{project_id}/locations/-")
+        response = request.execute()
+        return [function["name"].split("/")[-1] for function in response.get("functions", [])]
+    except:
+        return []
+
+def list_kubernetes_clusters(container_client, project_id):
+    try:
+        request = container_client.projects().locations().clusters().list(parent=f"projects/{project_id}/locations/-")
+        response = request.execute()
+        return [cluster["name"] for cluster in response.get("clusters", [])]
+    except:
+        return []
+
+def list_iam_service_accounts(iam_client, project_id):
+    try:
+        request = iam_client.projects().serviceAccounts().list(name=f"projects/{project_id}")
+        response = request.execute()
+        return [account["email"] for account in response.get("accounts", [])]
+    except:
+        return []
+
+def list_cloud_run_services(run_client, project_id):
+    try:
+        request = run_client.projects().locations().services().list(parent=f"projects/{project_id}/locations/-")
+        response = request.execute()
+        return [service["metadata"]["name"] for service in response.get("items", [])]
+    except:
+        return []
+
+def list_spanner_instances(spanner_client, project_id):
+    try:
+        request = spanner_client.projects().instances().list(parent=f"projects/{project_id}")
+        response = request.execute()
+        return [instance["name"] for instance in response.get("instances", [])]
+    except:
+        return []
+
+def list_memorystore_instances(memorystore_client, project_id):
+    try:
+        request = memorystore_client.projects().locations().instances().list(parent=f"projects/{project_id}/locations/-")
+        response = request.execute()
+        return [instance["name"] for instance in response.get("instances", [])]
+    except:
+        return []
+
+def list_filestore_instances(filestore_client, project_id):
+    try:
+        request = filestore_client.projects().locations().instances().list(parent=f"projects/{project_id}/locations/-")
+        response = request.execute()
+        return [instance["name"] for instance in response.get("instances", [])]
+    except:
+        return []
 
 def export_to_json(data, filename):
+    output = []
+    for service, resource in data.items():
+        if len(resource) != 0:
+            output.extend(resource)
+
     with open(filename + ".json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
+        json.dump(output, json_file, indent=4)
     print(f"Data exported to {filename}.json")
 
 def export_to_csv(data, filename):
@@ -59,10 +143,10 @@ def export_to_csv(data, filename):
 def export_to_xml(data, filename):
     root = ET.Element("GCPResources")
     for service, resources in data.items():
-        service_element = ET.SubElement(root, service)
-        for resource in resources:
-            resource_element = ET.SubElement(service_element, "Resource")
-            resource_element.text = resource
+        if len(resources) != 0:
+            output = ', '.join(resources)
+            service_element = ET.SubElement(root, service)
+            service_element.text = str(output)
     tree = ET.ElementTree(root)
     tree.write(filename + ".xml")
     print(f"Data exported to {filename}.xml")
@@ -82,14 +166,23 @@ def main():
     services = {
         "compute": (list_compute_instances, "Compute Instances"),
         "storage": (list_storage_buckets, "Storage Buckets"),
+        "sqladmin": (list_cloudsql_instances, "Cloud SQL Instances"),
+        "pubsub": (list_pubsub_topics, "Pub/Sub Topics"),
+        "cloudfunctions": (list_cloud_functions, "Cloud Functions"),
+        "container": (list_kubernetes_clusters, "Kubernetes Clusters"),
+        "iam": (list_iam_service_accounts, "IAM Service Accounts"),
+        "run": (list_cloud_run_services, "Cloud Run Services"),
+        "spanner": (list_spanner_instances, "Spanner Instances"),
+        "redis": (list_memorystore_instances, "Memorystore Instances"),
+        "file": (list_filestore_instances, "Filestore Instances"),
     }
     
     clients = {service: get_gcp_client(service, "v1", credentials) for service in services}
     results = {}
-    
+
     for service, (function, label) in services.items():
         results[label] = function(clients[service], args.project_id)
-    
+
     for service, resources in results.items():
         for resource in resources:
             print(f"{resource}")
